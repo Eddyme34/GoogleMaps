ce06021e8af30deb6e2e77cd12e1b3eb13328b0f
Nicholas Chan <nrchan@ucdavis.edu>
2022-03-06 22:56:33 -0800

    Add markers from list of locations

--- GoogleMaps/index.js
+++ GoogleMaps/index.js
@@ -1,16 +1,37 @@
 
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
   const silo = { lat: 38.5389171, lng: -121.7522203 };
-  // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
     center: silo,
   });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: silo,
-    map: map,
-  });
+  const locations = getLocations();
+  addMarkers(locations, map);
+}
+
+function getLocations() {
+  return [
+    {
+      position: new google.maps.LatLng(38.5389171, -121.7522203),
+      type: "trash"
+    },
+    {
+      position: new google.maps.LatLng(38.5389181, -121.7522253),
+      type: "recycle"
+    },
+    {
+      position: new google.maps.LatLng(38.5389271, -121.7522103),
+      type: "compost"
+    },
+  ]
+}
+
+function addMarkers(locations, map) {
+  for (const location of locations) {
+    new google.maps.Marker({
+      position: location.position,
+      map: map
+    })
+  }
 }
